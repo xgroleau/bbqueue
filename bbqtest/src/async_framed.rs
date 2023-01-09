def36c8ff659ed8cca698531ac38dfff19860eb6
xgroleau🐢 <xavgroleau@gmail.com>
2023-01-09 15:10:44 -0500

    Fix framed tests

--- bbqtest/src/async_framed.rs
+++ bbqtest/src/async_framed.rs
@@ -11,14 +11,14 @@ mod tests {
             let (mut prod, mut cons) = bb.try_split_framed().unwrap();
 
             // Create largeish grants
-            let mut wgr = prod.grant(127).unwrap();
+            let mut wgr = prod.grant_async(127).await.unwrap();
             for (i, by) in wgr.iter_mut().enumerate() {
                 *by = i as u8;
             }
             // Note: In debug mode, this hits a debug_assert
             wgr.commit(256);
 
-            let rgr = cons.read().unwrap();
+            let rgr = cons.read_async().await.unwrap();
             assert_eq!(rgr.len(), 127);
             for (i, by) in rgr.iter().enumerate() {
                 assert_eq!((i as u8), *by);
@@ -36,14 +36,14 @@ mod tests {
 
             for _ in 0..10_000 {
                 // Create largeish grants
-                if let Ok(mut wgr) = prod.grant(127) {
+                if let Ok(mut wgr) = prod.grant_async(127).await {
                     ctr += 1;
                     for (i, by) in wgr.iter_mut().enumerate() {
                         *by = i as u8;
                     }
                     wgr.commit(127);
 
-                    let rgr = cons.read().unwrap();
+                    let rgr = cons.read_async().await.unwrap();
                     assert_eq!(rgr.len(), 127);
                     for (i, by) in rgr.iter().enumerate() {
                         assert_eq!((i as u8), *by);
@@ -51,13 +51,13 @@ mod tests {
                     rgr.release();
                 } else {
                     // Create smallish grants
-                    let mut wgr = prod.grant(1).unwrap();
+                    let mut wgr = prod.grant_async(1).await.unwrap();
                     for (i, by) in wgr.iter_mut().enumerate() {
                         *by = i as u8;
                     }
                     wgr.commit(1);
 
-                    let rgr = cons.read().unwrap();
+                    let rgr = cons.read_async().await.unwrap();
                     assert_eq!(rgr.len(), 1);
                     for (i, by) in rgr.iter().enumerate() {
                         assert_eq!((i as u8), *by);
@@ -77,23 +77,23 @@ mod tests {
             let (mut prod, mut cons) = bb.try_split_framed().unwrap();
 
             // Create largeish grants
-            let mut wgr = prod.grant(128).unwrap();
+            let mut wgr = prod.grant_async(128).await.unwrap();
             for (i, by) in wgr.iter_mut().enumerate() {
                 *by = i as u8;
             }
             wgr.commit(255);
 
-            let mut wgr = prod.grant(64).unwrap();
+            let mut wgr = prod.grant_async(64).await.unwrap();
             for (i, by) in wgr.iter_mut().enumerate() {
                 *by = (i as u8) + 128;
             }
             wgr.commit(127);
 
-            let rgr = cons.read().unwrap();
+            let rgr = cons.read_async().await.unwrap();
             assert_eq!(rgr.len(), 128);
             rgr.release();
 
-            let rgr = cons.read().unwrap();
+            let rgr = cons.read_async().await.unwrap();
             assert_eq!(rgr.len(), 64);
             rgr.release();
         });
@@ -107,33 +107,33 @@ mod tests {
 
             for _ in 0..100_000 {
                 // Create largeish grants
-                let mut wgr = prod.grant(128).unwrap();
+                let mut wgr = prod.grant_async(128).await.unwrap();
                 for (i, by) in wgr.iter_mut().enumerate() {
                     *by = i as u8;
                 }
                 wgr.commit(13);
 
-                let mut wgr = prod.grant(64).unwrap();
+                let mut wgr = prod.grant_async(64).await.unwrap();
                 for (i, by) in wgr.iter_mut().enumerate() {
                     *by = (i as u8) + 128;
                 }
                 wgr.commit(7);
 
-                let mut wgr = prod.grant(32).unwrap();
+                let mut wgr = prod.grant_async(32).await.unwrap();
                 for (i, by) in wgr.iter_mut().enumerate() {
                     *by = (i as u8) + 192;
                 }
                 wgr.commit(0);
 
-                let rgr = cons.read().unwrap();
+                let rgr = cons.read_async().await.unwrap();
                 assert_eq!(rgr.len(), 13);
                 rgr.release();
 
-                let rgr = cons.read().unwrap();
+                let rgr = cons.read_async().await.unwrap();
                 assert_eq!(rgr.len(), 7);
                 rgr.release();
 
-                let rgr = cons.read().unwrap();
+                let rgr = cons.read_async().await.unwrap();
                 assert_eq!(rgr.len(), 0);
                 rgr.release();
             }
@@ -148,7 +148,7 @@ mod tests {
 
             for _ in 0..100 {
                 {
-                    let mut wgr = prod.grant(64).unwrap();
+                    let mut wgr = prod.grant_async(64).await.unwrap();
                     wgr.to_commit(64);
                     for (i, by) in wgr.iter_mut().enumerate() {
                         *by = i as u8;
@@ -157,7 +157,7 @@ mod tests {
                 }
 
                 {
-                    let mut rgr = cons.read().unwrap();
+                    let mut rgr = cons.read_async().await.unwrap();
                     rgr.auto_release(true);
                     let rgr = rgr;
 
